eaa299c2fbcfca3b10221951e3a8dbbcc444826c
Psyk0loge <SimonGoetz99@gmx.de>
2023-12-31 16:35:50 +0100

    adept weather Backend

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("worked")
-    fetch('www.monnem-wetter.de/weather') // Replace with your actual backend URL
+    fetch('/weather') // Replace with your actual backend URL
         .then(response => response.json())
         .then(data => {
             document.getElementById('temperature').textContent = data.temperature;
